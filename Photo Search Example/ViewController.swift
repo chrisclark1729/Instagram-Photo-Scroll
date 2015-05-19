45ec075bd4bb3e5a448cd1226858dbf1231811a2
Chris Clark <chrismclark@chrismclark-macbookpro2.roam.corp.google.com>
2015-05-19 09:46:28 -0700

    Update View Controller

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "https://api.instagram.com/v1/tags/chefcurry/media/recent?client_id=f53244354bd9465097e541dbc45919e1",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+               // println("JSON: " + responseObject.description)
+                if let dataArray = responseObject["data"] as? [AnyObject] {
+                    var urlArray:[String] = []
+                    for dataObject in dataArray {
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            urlArray.append(imageURLString)
+                        }
+                    }
+                    //display urlArray in ScrollView
+                    self.scrollView.contentSize = CGSizeMake(320, 320 * CGFloat(dataArray.count))
+                    for var i = 0; i < urlArray.count; i++ {
+                        let imageView = UIImageView(frame: CGRectMake(0, 320*CGFloat(i), 320, 320))     //1
+                        imageView.setImageWithURL( NSURL(string: urlArray[i]))                          //2
+                        self.scrollView.addSubview(imageView)                                     //5
+                        }
+                }
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
