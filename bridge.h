799c8f2d02a3ec244988075dfe26a489ec81169f
Chris Clark <chrismclark@chrismclark-macbookpro2.roam.corp.google.com>
2015-05-19 09:46:07 -0700

    Add bridge.h

---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Chris Clark on 5/18/15.
+//  Copyright (c) 2015 Chris Clark. All rights reserved.
+//
+
+#ifndef Photo_Search_Example_bridge_h
+#define Photo_Search_Example_bridge_h
+#import "AFNetworking/AFNetworking.h"
+#import "UIKit+AFNetworking/UIKit+AFNetworking.h"
+
+#endif
